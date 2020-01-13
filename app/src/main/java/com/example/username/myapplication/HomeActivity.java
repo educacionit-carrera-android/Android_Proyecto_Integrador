ee662ee9764fc5d3e0287d7dd87acbcf4c71c630
carrera.android <carrera.android@educacionit.com>
2020-01-12 23:03:54 -0300

    Add lab 3 from class 3

--- app/src/main/java/com/example/username/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/username/myapplication/HomeActivity.java
@@ -2,20 +2,45 @@ package com.example.username.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ListView lvLibros;
+    private LibrosAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        saludarUsuario();
+        setupAdapter();
+    }
 
+    private void setupAdapter() {
+        lvLibros = findViewById(R.id.lvLibros);
+        adapter = new LibrosAdapter(getLibros());
+        lvLibros.setAdapter(adapter);
+    }
+
+    private List<Libro> getLibros() {
+        List<Libro> libros = new ArrayList<>();
+        libros.add(new Libro(1, "Canción de hielo y fuego", "George R. R. Martin"));
+        libros.add(new Libro(2, "Harry Potter y el cáliz de fuego", "J. K. Rowling"));
+        libros.add(new Libro(3, "Los juegos del hambre en llamas", "Suzanne Collins"));
+        libros.add(new Libro(4, "Maze runner", "James Dashner"));
+        return libros;
+    }
+
+    private void saludarUsuario() {
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             String usuario = bundle.getString("USUARIO");
             Toast.makeText(HomeActivity.this, "Bienvenido " + usuario, Toast.LENGTH_SHORT).show();
         }
-
     }
 }
---
+++ app/src/main/java/com/example/username/myapplication/LibrosAdapter.java
@@ -0,0 +1,52 @@
+package com.example.username.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class LibrosAdapter extends BaseAdapter {
+
+    private List<Libro> libros;
+
+    public LibrosAdapter(List<Libro> libros) {
+        this.libros = libros;
+    }
+
+    @Override
+    public int getCount() {
+        return libros.size();
+    }
+
+    @Override
+    public Libro getItem(int position) {
+        return libros.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return libros.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //Asignamos al objeto View la vista que inflamos
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.item_libro, parent, false);
+        //Obtenemos el libro a partir de la posición que nos viene
+        Libro libro = libros.get(position);
+
+        //Obtenemos los TextViews de nuestra vista (el layout item_libro)
+        TextView txtNombre = view.findViewById(R.id.txtNombre);
+        TextView txtAutor = view.findViewById(R.id.txtAutor);
+
+        txtNombre.setText(libro.getNombre());
+        txtAutor.setText(libro.getAutor());
+
+        return view;
+    }
+}
