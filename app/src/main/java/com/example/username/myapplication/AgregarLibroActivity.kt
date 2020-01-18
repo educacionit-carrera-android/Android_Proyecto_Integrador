86e08904067e64906e4550a3300b58159e7d1b16
carrera.android <carrera.android@educacionit.com>
2020-01-18 13:53:03 -0300

    Add lab 3 from class 2

--- app/src/main/java/com/example/username/myapplication/AgregarLibroActivity.kt
+++ app/src/main/java/com/example/username/myapplication/AgregarLibroActivity.kt
@@ -2,49 +2,17 @@ package com.example.username.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import java.sql.SQLException
 
 class AgregarLibroActivity : AppCompatActivity() {
-    private lateinit var etNombreLibro: EditText
-    private lateinit var etAutor: EditText
-    private lateinit var btnGuardar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_libro)
-        setupUI()
-        btnGuardar.setOnClickListener {
-            if (datosValidos()) {
-                val libro = Libro()
-                libro.nombre = etNombreLibro.text.toString()
-                libro.autor = etAutor.text.toString()
-                try {
-                    LibroManager.getInstance(this@AgregarLibroActivity).agregarLibro(libro)
-                    finish()
-                } catch (e: SQLException) {
-                    e.printStackTrace()
-                    Toast.makeText(this@AgregarLibroActivity, "No se pudo agregar el libro", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this@AgregarLibroActivity, "Completar todos los campos", Toast.LENGTH_SHORT).show()
-            }
-        }
+
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, AgregarLibroFragment(), AgregarLibroFragment::javaClass.name)
+                .commit()
     }
 
-    private fun datosValidos(): Boolean {
-        var datosValidos = true
-        if (etNombreLibro.text.toString().isEmpty() || etAutor.text.toString().isEmpty()) {
-            datosValidos = false
-        }
-        return datosValidos
-    }
-
-    private fun setupUI() {
-        etNombreLibro = findViewById(R.id.etNombreLibro)
-        etAutor = findViewById(R.id.etAutor)
-        btnGuardar = findViewById(R.id.btnGuardar)
-    }
 }
---
+++ app/src/main/java/com/example/username/myapplication/AgregarLibroFragment.kt
@@ -0,0 +1,62 @@
+package com.example.username.myapplication
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.sql.SQLException
+
+class AgregarLibroFragment : Fragment() {
+
+    private lateinit var etNombreLibro: EditText
+    private lateinit var etAutor: EditText
+    private lateinit var btnGuardar: Button
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_agregar_libro, container, false)
+        setupUI(view)
+        initializeBtnGuardar()
+
+        return view
+    }
+
+
+    private fun setupUI(view: View) {
+        etNombreLibro = view.findViewById(R.id.etNombreLibro)
+        etAutor = view.findViewById(R.id.etAutor)
+        btnGuardar = view.findViewById(R.id.btnGuardar)
+    }
+
+    private fun initializeBtnGuardar() {
+        btnGuardar.setOnClickListener {
+            if (datosValidos()) {
+                val libro = Libro()
+                libro.nombre = etNombreLibro.text.toString()
+                libro.autor = etAutor.text.toString()
+                try {
+                    LibroManager.getInstance(context).agregarLibro(libro)
+                    activity?.finish()
+                } catch (e: SQLException) {
+                    e.printStackTrace()
+                    Toast.makeText(context, "No se pudo agregar el libro", Toast.LENGTH_SHORT).show()
+                }
+
+            } else {
+                Toast.makeText(context, "Completar todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun datosValidos(): Boolean {
+        var datosValidos = true
+        if (etNombreLibro.text.toString().isEmpty() || etAutor.text.toString().isEmpty()) {
+            datosValidos = false
+        }
+        return datosValidos
+    }
+
+}
