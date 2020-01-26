f6999d616e8873417e2105eb48365a2ab06098c0
carrera.android <carrera.android@educacionit.com>
2020-01-26 17:45:35 -0300

    Migrate to Android X and remove tests

--- app/src/main/java/com/example/username/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/username/myapplication/HomeActivity.java
@@ -2,14 +2,15 @@ package com.example.username.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
