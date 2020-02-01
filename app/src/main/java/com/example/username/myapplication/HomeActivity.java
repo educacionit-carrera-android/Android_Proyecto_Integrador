bbf0a585f55bcad4c6d6dc1cea473b4343e968d9
carrera.android <carrera.android@educacionit.com>
2020-02-01 01:47:53 -0300

    Replace ListView with RecyclerView

--- app/src/main/java/com/example/username/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/username/myapplication/HomeActivity.java
@@ -4,20 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ListView lvLibros;
+    private RecyclerView rvLibros;
     private LibrosAdapter adapter;
     private Toolbar toolbar;
 
@@ -37,16 +35,14 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void setupAdapter() {
-        lvLibros = findViewById(R.id.lvLibros);
-        adapter = new LibrosAdapter(getLibros());
-        lvLibros.setAdapter(adapter);
-        lvLibros.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        rvLibros = findViewById(R.id.rvLibros);
+        adapter = new LibrosAdapter(getLibros(), new LibrosAdapter.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Libro libro = adapter.getItem(position);
+            public void onItemClick(Libro libro) {
                 Toast.makeText(HomeActivity.this, libro.getNombre(), Toast.LENGTH_SHORT).show();
             }
         });
+        rvLibros.setAdapter(adapter);
     }
 
     @Override
